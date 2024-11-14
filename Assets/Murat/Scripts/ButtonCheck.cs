429340b5bb8d2089b7f55bbb07ba8c8427d4bbd9
MuratAr448 <144346044+MuratAr448@users.noreply.github.com>
2024-11-14 09:42:37 +0100

    see image while chosing

---
+++ Assets/Murat/Scripts/ButtonCheck.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCheck : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown killerDropdown;
+    [SerializeField] private TMP_Dropdown roomDropdown;
+    [SerializeField] private TMP_Dropdown weaponDropdown;
+
+    [SerializeField] private Image imageKiller;
+    [SerializeField] private Image imageRoom;
+    [SerializeField] private Image imageWeapon;
+
+    [SerializeField] private List<Sprite> imagePeopleList;
+    [SerializeField] private List<Sprite> imageRoomList;
+    [SerializeField] private List<Sprite> imageWeaponList;
+    WinSystem winSystem;
+    private void Start()
+    {
+        winSystem = FindObjectOfType<WinSystem>();
+    }
+    private void Update()
+    {
+        imageKiller.sprite = imagePeopleList[killerDropdown.value];
+        imageRoom.sprite = imageRoomList[roomDropdown.value];
+        imageWeapon.sprite = imageWeaponList[weaponDropdown.value];
+    }
+    public void Check()
+    {
+        if (RoomCheck())
+        {
+            //corret roomK
+        }
+        else
+        {
+            //incorret roomK
+        }
+
+        if (WaponCheck())
+        {
+            //corret weaponK
+        }
+        else
+        {
+            //incorret weaponK
+        }
+
+        if (KillerCheck())
+        {
+            //corret person
+        }
+        else
+        {
+            //incorret person
+        }
+
+        if (RoomCheck() && WaponCheck() && KillerCheck())
+        {
+            Debug.Log("you win");
+        }
+        else
+        {
+            Debug.Log("you lose");
+        }
+    }
+
+    bool KillerCheck()
+    {
+        if (winSystem.People[killerDropdown.value] == winSystem.People[winSystem.killerK])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool RoomCheck()
+    {
+        if (winSystem.Rooms[roomDropdown.value] == winSystem.Rooms[winSystem.roomK])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool WaponCheck()
+    {
+        if (winSystem.Weapons[weaponDropdown.value] == winSystem.Weapons[winSystem.weaponK])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
+
--- Assets/Murat/Scripts/WinSystem.cs
+++ Assets/Murat/Scripts/WinSystem.cs
@@ -1,20 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class WinSystem : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> Rooms = new List<GameObject>();
-    [SerializeField] private List<GameObject> Weapons = new List<GameObject>();
-    [SerializeField] private List<GameObject> People = new List<GameObject>();
+    public List<GameObject> People = new List<GameObject>();
+    public List<GameObject> Rooms = new List<GameObject>();
+    public List<GameObject> Weapons = new List<GameObject>();
 
-    [SerializeField] private int roomK;
-    [SerializeField] private int killerK;
-    [SerializeField] private int weaponK;
-
-    public int chosenRoom = -1;
-    public int chosenKiller = -1;
-    public int chosenWeapon = -1;
+    public int killerK;
+    public int roomK;
+    public int weaponK;
 
     private static WinSystem instanse;
     private void Awake()
@@ -27,94 +25,23 @@ public class WinSystem : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        DontDestroyOnLoad(this.gameObject); 
+        DontDestroyOnLoad(this.gameObject);
     }
     void Start()
     {
-        rand();
+       rand();
     }
 
     void Update()
     {
-        
+
     }
     void rand()
     {
-        roomK = Random.Range(0, Rooms.Count);
         killerK = Random.Range(0, People.Count);
+        roomK = Random.Range(0, Rooms.Count);
         weaponK = Random.Range(0, Weapons.Count);
     }
-    public void Check()
-    {
-        if (RoomCheck())
-        {
-            //corret roomK
-        }
-        else
-        {
-            //incorret roomK
-        }
-
-        if (WaponCheck())
-        {
-            //corret weaponK
-        }
-        else
-        {
-            //incorret weaponK
-        }
-
-        if (KillerCheck())
-        {
-            //corret person
-        }
-        else
-        {
-            //incorret person
-        }
-
-        if (RoomCheck()&& WaponCheck() && KillerCheck())
-        {
-            //you win
-        }else
-        {
-            //you lose
-        }
-    }
-
-    bool RoomCheck()
-    {
-        if (Rooms[chosenRoom] == Rooms[roomK])
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    bool WaponCheck()
-    {
-        if (Weapons[chosenWeapon] == Weapons[weaponK])
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    bool KillerCheck()
-    {
-        if (People[chosenKiller] == People[killerK])
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
 }
+
+
