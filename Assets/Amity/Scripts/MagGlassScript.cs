8611ebb71d297cfca4e6a993448be5504f7bd149
AmityBB <amity.b.bolleurs@gmail.com>
2024-11-14 12:32:36 +0100

    vergrootglas werkt

--- Assets/Amity/Scripts/MagGlassScript.cs
+++ Assets/Amity/Scripts/MagGlassScript.cs
@@ -1,38 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.PostProcessing;
 
 public class MagGlassScript : MonoBehaviour
 {
     [SerializeField]
     private float Distance;
-    // Start is called before the first frame update
-    void Start()
+    public float TargetZoom;
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    [SerializeField] 
+    private Camera MagView;
+    
+
+    private void Start()
     {
-        
+        TargetZoom = UnityEngine.Random.Range(-34.01004f, -36.81f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
-        if (Input.GetMouseButton(0))
+        if(transform.position.z > -34.01004)
         {
-            Vector3 point = new Vector3();
-            Vector2 mousePos = new Vector2();
-
-            mousePos.x = Input.mousePosition.x;
-            mousePos.y = Camera.main.pixelHeight/10 + Input.mousePosition.y;
-
-            point = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, Camera.main.nearClipPlane));
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if(Physics.Raycast(ray, out hit, 100))
-            {
-                gameObject.transform.position = point + (Camera.main.transform.forward * Distance);
-            }
+            transform.position = new Vector3(transform.position.x, transform.position.y, -34.01004f);
         }
+        if(transform.position.z < -36.81)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -36.81f);
+        }
+        if(Math.Abs(transform.position.z - TargetZoom) < 0.05)
+        {
+            MagView.GetComponent<PostProcessVolume>().enabled = false;
+        }
+        else
+        {
+            MagView.GetComponent<PostProcessVolume>().enabled = true;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
     }
 }
