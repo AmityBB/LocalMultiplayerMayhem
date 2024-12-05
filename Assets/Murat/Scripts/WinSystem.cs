8e2b144d03b3ce06e411204304bae68aa2a6bdec
MuratAr448 <144346044+MuratAr448@users.noreply.github.com>
2024-12-05 08:35:00 +0100

    blood/handprint easy

--- Assets/Murat/Scripts/WinSystem.cs
+++ Assets/Murat/Scripts/WinSystem.cs
@@ -30,8 +30,6 @@ public class WinSystem : MonoBehaviour
     [SerializeField] private List<Sprite> spriteRoomList;
     [SerializeField] private List<Sprite> spriteWeaponList;
 
-    public GameObject fingerPrintPanel;
-    public GameObject bloodPanel;
     public List<GameObject> roomBloodPrintPanels;
 
     public float choosTime = 0;
@@ -64,59 +62,7 @@ public class WinSystem : MonoBehaviour
         killerK = Random.Range(0, People.Count);
         roomK = Random.Range(0, Rooms.Count);
         weaponK = Random.Range(0, Weapons.Count);
-        switch (roomK)
-        {
-            case 0:
-                Instantiate(fingerPrintPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(3, 43), roomBloodPrintPanels[roomK].transform.position.y+0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(0, 33)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                Instantiate(bloodPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(3, 43), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(0, 33)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                break;
-            case 1:
-                Instantiate(fingerPrintPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(3, 53), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(3, 23)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                Instantiate(bloodPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(3, 53), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(3, 23)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                break;
-            case 2:
-                int rand3 = Random.Range(1,4);
-                switch (rand3)
-                {
-                    case 0:
-                        Instantiate(fingerPrintPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(3, 26), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(3, 33)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                        Instantiate(bloodPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(3, 26), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(3, 33)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                        break;
-                    case 1:
-                        Instantiate(fingerPrintPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(32, 62), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(-7, 13)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                        Instantiate(bloodPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(32, 62), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(-7, 13)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                        break;
-                    case 2:
-                        Instantiate(fingerPrintPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(32, 62), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(16, 26)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                        Instantiate(bloodPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(32, 62), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(16, 26)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                        break;
-                    default:
-                        break;
-                }
-                break;
-            case 3:
-                Instantiate(fingerPrintPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(3, 34), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(3, 21)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                Instantiate(bloodPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(3, 34), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(3, 21)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                break;
-            case 4:
-                int rand5 = Random.Range(1, 4);
-                switch (rand5)
-                {
-                    case 0:
-                        Instantiate(fingerPrintPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(3, 47), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(3, 33)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                        Instantiate(bloodPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(3, 47), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(3, 33)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                        break;
-                    case 1:
-                        Instantiate(fingerPrintPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(47, 97), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(13, 33)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                        Instantiate(bloodPanel, new Vector3(roomBloodPrintPanels[roomK].transform.position.x + Random.Range(47, 97), roomBloodPrintPanels[roomK].transform.position.y + 0.13f, roomBloodPrintPanels[roomK].transform.position.z + Random.Range(13, 33)), Quaternion.Euler(90, Random.Range(0, 360), Random.Range(0, 360)));
-                        break;
-                    default:
-                        break;
-                }
-                break;
-            default:
-                break;
-        }
+        roomBloodPrintPanels[roomK].SetActive(true);
     }
     private void Update()
     {
